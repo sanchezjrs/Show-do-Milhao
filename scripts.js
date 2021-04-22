3d8e6b43c9f7a94a3e950e34fc5b799506b719bc
Antonio Sanchez <82811454+sanchezjrs@users.noreply.github.com>
2021-04-21 22:08:15 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var operacao = prompt("Olá, seja bem vindo ao Show do Milhão.\n Pergunta valendo um curso na Alura.\n Quem são os instrutores das aulas da ImersãoDev 2021\n 1- É o Guilherme e a Rafa?\n 2- É o Chaves e a Chiquinha?\n 3- É a Monica e o Cebolinha?")
+
+if(operacao == 1){
+  document.write("<h2>Certa resposta!</h2>")
+}
+  else if(operacao == 2){
+  document.write("<h2>Não, a resposta está errada!</h2>")
+}
+  else if(operacao == 3){
+  document.write("<h2>Não, a resposta está errada!</h2>")
+}
+  else{
+  document.write("<h2>Resposta invalida!</h2>")
+  }
